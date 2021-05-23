693c5fe8b37218ccbcdd4409cffbf95b3a185870
sunil sharma <sunil68895@gmail.com>
2021-05-23 17:43:25 +0530

    update auth

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -45,26 +45,57 @@ exports.signup = (req, res) => {
   });
 };
 
+// exports.signin = (req, res) => {
+//   User.findOne({ email: req.body.email }).exec(async (error, user) => {
+//     if (error) return res.status(400).json({ error });
+//     if (user) {
+//       const isPassword = await user.authenticate(req.body.password);
+//       if (isPassword && user.role === "user") {
+//         // const token = jwt.sign(
+//         //   { _id: user._id, role: user.role },
+//         //   process.env.JWT_SECRET,
+//         //   { expiresIn: "1d" }
+//         // );
+//         const token = generateJwtToken(user._id, user.role);
+//         const { _id, firstName, lastName, email, role, fullName } = user;
+//         res.status(200).json({
+//           token,
+//           user: { _id, firstName, lastName, email, role, fullName },
+//         });
+//       } else {
+//         return res.status(400).json({
+//           message: "Something went wrong",
+//         });
+//       }
+//     } else {
+//       return res.status(400).json({ message: "Something went wrong" });
+//     }
+//   });
+// };
+
 exports.signin = (req, res) => {
   User.findOne({ email: req.body.email }).exec(async (error, user) => {
     if (error) return res.status(400).json({ error });
     if (user) {
       const isPassword = await user.authenticate(req.body.password);
-      if (isPassword && user.role === "user") {
-        // const token = jwt.sign(
-        //   { _id: user._id, role: user.role },
-        //   process.env.JWT_SECRET,
-        //   { expiresIn: "1d" }
-        // );
-        const token = generateJwtToken(user._id, user.role);
+      if (
+        isPassword &&
+        (user.role === "user")
+      ) {
+        const token = jwt.sign(
+          { _id: user._id, role: user.role },
+          process.env.JWT_SECRET,
+          { expiresIn: "1d" }
+        );
         const { _id, firstName, lastName, email, role, fullName } = user;
+        res.cookie("token", token, { expiresIn: "1d" });
         res.status(200).json({
           token,
           user: { _id, firstName, lastName, email, role, fullName },
         });
       } else {
         return res.status(400).json({
-          message: "Something went wrong",
+          message: "Invalid Password",
         });
       }
     } else {
