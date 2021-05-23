1b57645525aa16d39758ec9ddd7f87db1ed9d1ab
sunil68895 <72141073+sunil68895@users.noreply.github.com>
2021-05-23 18:31:34 +0530

    Update index.server.js

--- src/index.server.js
+++ src/index.server.js
@@ -35,6 +35,10 @@ mongoose
     console.log("database connected");
   });
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "uploads")));
 app.use("/api", authRoutes);
